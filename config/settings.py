b7a3b79cca74c14b7eaefa359f3744aa575541f1
Do Van Thanh <thanhdv7454@co-well.vn>
2022-12-15 14:12:33 +0700

    up

--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+import sys
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
