c8caa95f6c4b834c9ad1e54a01570627246cf4ca
Do Van Thanh <thanhdv7454@co-well.vn>
2022-12-15 15:04:11 +0700

    up

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,12 +1,8 @@
-import datetime
-
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-
-from common.utils import get_time_now
 
 
 GENDER_CHOICES = (
